bb94b719a6530dd6da2a8b7452e44598028e4e3e
salem <salem.mnassri@esprit.tn>
2018-11-22 01:21:51 +0100

    Ngx-print directive core

--- projects/ngx-print/src/lib/ngx-print.directive.ts
+++ projects/ngx-print/src/lib/ngx-print.directive.ts
@@ -1,10 +1,27 @@
-import { Directive } from '@angular/core';
+import { Directive, HostListener, Input } from '@angular/core';
 
 @Directive({
-  selector: '[NgxPrint]'
+  selector: 'button[ngxPrint]'
 })
 export class NgxPrintDirective {
 
-  constructor() { }
+  @Input() printSectionId: string;
 
+  @HostListener('click', ['$event.target'])
+
+  public print(btn): void {
+    let printContents, popupWin;
+    printContents = document.getElementById(this.printSectionId).innerHTML;
+    popupWin = window.open('', '_blank', 'top=0,left=0,height=100%,width=auto');
+    popupWin.document.open();
+    popupWin.document.write(`
+      <html>
+        <head>
+          <title>Print tab</title>
+        </head>
+    <body onload="window.print();window.close()">${printContents}</body>
+      </html>`
+    );
+    popupWin.document.close();
+ }
 }
