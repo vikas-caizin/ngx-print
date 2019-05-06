fb0ac8cce3d802132ad653ab8ea91b124cbfdd8c
Deep Lotia <deep.lotia>
2019-05-06 22:09:04 +0200

    Added styles feature by passing the css file names to enable multple styles

--- src/lib/ngx-print.directive.ts
+++ src/lib/ngx-print.directive.ts
@@ -1,10 +1,8 @@
-import { Directive, HostListener, Input } from '@angular/core';
+import { Directive, HostListener, Input } from "@angular/core";
 @Directive({
-  selector: 'button[ngxPrint]'
+  selector: "button[ngxPrint]"
 })
-
 export class NgxPrintDirective {
-
   private _printStyle = [];
 
   /**
@@ -27,27 +25,29 @@ export class NgxPrintDirective {
    * @memberof NgxPrintDirective
    */
   @Input()
-  set printStyle(values: {[key: string]: {[key: string]: string}}) {
+  set printStyle(values: { [key: string]: { [key: string]: string } }) {
     for (var key in values) {
       if (values.hasOwnProperty(key)) {
-      this._printStyle.push((key + JSON.stringify(values[key])).replace(/['"]+/g, ''));
+        this._printStyle.push(
+          (key + JSON.stringify(values[key])).replace(/['"]+/g, "")
+        );
       }
     }
     this.returnStyleValues();
   }
 
-/**
- *
- *
- * @returns the string that create the stylesheet which will be injected
- * later within <style></style> tag.
- *
- * -join/replace to transform an array objects to css-styled string
- *
- * @memberof NgxPrintDirective
- */
-private returnStyleValues() {
-  return this._printStyle.join(' ').replace(',',';');
+  /**
+   *
+   *
+   * @returns the string that create the stylesheet which will be injected
+   * later within <style></style> tag.
+   *
+   * -join/replace to transform an array objects to css-styled string
+   *
+   * @memberof NgxPrintDirective
+   */
+  private returnStyleValues() {
+    return this._printStyle.join(" ").replace(",", ";");
   }
 
   /**
@@ -55,23 +55,57 @@ private returnStyleValues() {
    *
    * @memberof NgxPrintDirective
    */
-  @HostListener('click', ['$event.target'])
+  private _styleSheetFile = "";
+
+  /**
+   * @memberof NgxPrintDirective
+   * @param cssList
+   */
+  @Input()
+  set styleSheetFile(cssList: string) {
+    let linkTagFn = cssFileName =>
+      `<link rel="stylesheet" type="text/css" href="${cssFileName}">`;
+    if (cssList.indexOf(",") !== -1) {
+      const valueArr = cssList.split(",");
+      for (let val of valueArr) {
+        this._styleSheetFile = this._styleSheetFile + linkTagFn(val);
+      }
+    } else {
+      this._styleSheetFile = linkTagFn(cssList);
+    }
+  }
+
+  /**
+   * @returns string which contains the link tags containing the css which will
+   * be injected later within <head></head> tag.
+   *
+   */
+  private returnStyleSheetLinkTags() {
+    return this._styleSheetFile;
+  }
+
+  /**
+   *
+   *
+   * @memberof NgxPrintDirective
+   */
+  @HostListener("click", ["$event.target"])
   public print(): void {
     let printContents, popupWin;
     printContents = document.getElementById(this.printSectionId).innerHTML;
-    popupWin = window.open('', '_blank', 'top=0,left=0,height=100%,width=auto');
+    popupWin = window.open("", "_blank", "top=0,left=0,height=100%,width=auto");
     popupWin.document.open();
     popupWin.document.write(`
       <html>
         <head>
-          <title>${this.printTitle ? this.printTitle : ''}</title>
+          <title>${this.printTitle ? this.printTitle : ""}</title>
+          ${this.returnStyleSheetLinkTags()}
           <style>
             ${this.returnStyleValues()}
           </style>
         </head>
     <body onload="window.print();window.close()">${printContents}</body>
-      </html>`
-    );
+      </html>`);
     popupWin.document.close();
- }
+  }
 }
